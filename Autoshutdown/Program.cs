4bafa46aa213235c961726ff15dd98071564d88d
Mutuku1998 <martinmutuku1998@gmail.com>
2025-03-02 07:46:54 +0300

    autoshutdown service

---
+++ Autoshutdown/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            DateTime now = DateTime.Now;
+            Console.Clear();
+            Console.WriteLine("Current Time: " + now.ToString("hh:mm:ss tt"));
+
+            DateTime shutdownTime = DateTime.Today.AddHours(17); 
+
+            if (now >= shutdownTime && now < shutdownTime.AddMinutes(1))
+            {
+                Console.WriteLine("It's 5 PM! Shutting down the computer...");
+                Process.Start("shutdown", "/s /t 0");
+                break;
+            }
+
+            Thread.Sleep(60000); 
+        }
+    }
+}
